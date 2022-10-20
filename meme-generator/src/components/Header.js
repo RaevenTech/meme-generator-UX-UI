42d06858d38d73791abccf15ec595675ffac79df
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-10-20 11:34:43 +0100

    header complete

--- meme-generator/src/components/Header.js
+++ meme-generator/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./header/header.module.css";
+import { GiWizardFace } from "react-icons/gi";
 
 const Header = () => {
-    return <div className={styles.main}>Header</div>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.logo}>
+                <h1>
+                    <GiWizardFace className={styles.wizard} />
+                </h1>
+                <h1>
+                    <span className={styles.logo_text}>Meme Generator</span>
+                </h1>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
